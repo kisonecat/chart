cfaf4e201058a8a1c93d6ee27699a44f6a2042cb
Jim Fowler <fowler@math.osu.edu>
2022-05-24 21:40:23 -0400

    sending jwt on a token endpoint

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Main where
 
@@ -21,12 +22,19 @@ import           Control.Concurrent.STM.TVar (TVar, newTVar, readTVar,
 
 import           Control.Exception           (bracket)
 import           Control.Monad.IO.Class      (liftIO)
-import           Control.Monad.Except        (liftEither, throwError)
+import           Control.Monad.Except        (liftEither, throwError, runExceptT)
 import           Control.Monad.STM           (atomically)
 import           Control.Monad.Trans.Reader  (ReaderT, ask, asks, runReaderT)
 
 import           GHC.Generics                (Generic)
 import           Network.Wai.Handler.Warp    (run)
+
+import qualified Crypto.JOSE.JWK as JOSE
+import Crypto.JOSE 
+import Crypto.JWT (signClaims, SignedJWT)
+import Crypto.JOSE.JWS (newJWSHeader)
+
+import Network.HTTP.Media ((//), (/:))
 
 import System.Environment (lookupEnv)
 
@@ -40,28 +48,48 @@ import           Data.Aeson                  (FromJSON, ToJSON)
 import Data.Text ( Text )  
 import qualified Database.Redis as R
 import Data.String.Conversions (cs)
+import qualified Data.ByteString as BS
 
-data User = User { username :: !Text
-                 , chart :: !Text
+data AuthenticatedUser = AuthenticatedUser { name :: !Text
+                                           , chart :: !Text
+                                           }
+                       deriving (Show, Generic)
+
+data User = User { userId :: !Text 
+                 , firstName :: !Text
+                 , lastName :: !Text
+                 , email :: !Text
                  }
           deriving (Show, Generic)
 
-instance FromJSON User
-instance ToJSON User 
-instance ToJWT User
-instance FromJWT User
+instance FromJSON AuthenticatedUser
+instance ToJSON AuthenticatedUser 
+instance ToJWT AuthenticatedUser
+instance FromJWT AuthenticatedUser
+
+data JSONWebToken  
+  
+instance Accept JSONWebToken where
+   contentType _ = "application" // "jwt" 
+
+instance {-# OVERLAPPING #-} ToCompact a => MimeRender JSONWebToken a where
+    mimeRender _ = encodeCompact
 
 -- this is just a fake stub for now
 authCheck :: Configuration -> R.Connection
           -> BasicAuthData
-          -> IO (AuthResult User)
+          -> IO (AuthResult AuthenticatedUser)
 authCheck config _connection (BasicAuthData login _password) = pure $
-  maybe SAS.Indefinite Authenticated $ Just $ User { username = cs login, chart = cs $ getHostname config }
+  maybe SAS.Indefinite Authenticated $ Just $ AuthenticatedUser { name = cs login, chart = cs $ getHostname config }
 
-type instance BasicAuthCfg = BasicAuthData -> IO (AuthResult User)
+type instance BasicAuthCfg = BasicAuthData -> IO (AuthResult AuthenticatedUser)
 
-instance FromBasicAuthData User where
+instance FromBasicAuthData AuthenticatedUser where
   fromBasicAuthData authData authCheckFunction = authCheckFunction authData
+
+type UserCrudAPI = Get '[JSON] User :<|> (ReqBody '[JSON] User :> (Put '[JSON] User :<|> Patch '[JSON] User))
+type UserTokenAPI = SAS.Auth '[SA.BasicAuth] AuthenticatedUser :> ( ("token" :> Get '[JSONWebToken] SignedJWT ) :<|> ("authorize" :> Get '[JSON] ()) )
+type UserAPI = SAS.Auth '[SA.JWT, SA.BasicAuth] AuthenticatedUser :> "users" :> Capture "username" Text :> (UserTokenAPI :<|> UserCrudAPI)
 
 newtype Book = Book String deriving (Show, Generic)
 instance ToJSON Book
@@ -70,11 +98,11 @@ instance FromJSON Book
 type GetBooks = Get '[JSON] [Book]
 
 type SecureBooksAPI =
-  SAS.Auth '[SA.JWT, SA.BasicAuth] User :> Get '[JSON] [Book]
+  SAS.Auth '[SA.JWT, SA.BasicAuth] AuthenticatedUser :> Get '[JSON] [Book]
 
 type BooksAPI = ("books" :> GetBooks) :<|> ("secure" :> SecureBooksAPI )
 
-api :: Proxy BooksAPI
+api :: Proxy UserTokenAPI 
 api = Proxy
 
 data AppCtx = AppCtx { getConfiguration :: Configuration
@@ -88,19 +116,47 @@ runRedis redis = do
    conn <- asks getConnection
    liftIO $ R.runRedis conn redis
 
-server :: SAS.CookieSettings -> SAS.JWTSettings -> ServerT BooksAPI AppM
-server cookies jwt = getBooks :<|> getSecure 
-  where getBooks :: AppM [Book]
-        getBooks = do
-          hello <- runRedis $ do
-            R.get "hello"
-          case hello of
-            Right result -> return [Book $ show result] 
-            Left _ -> throwError err500
-        getSecure :: AuthResult User -> AppM [Book]
-        getSecure (SAS.Authenticated _adminUser)  = pure [] 
-        getSecure _ = throwError err401 
 
+
+bestAlg :: AuthenticatedUser -> SAS.JWTSettings -> IO (Either Crypto.JOSE.Error Crypto.JWT.SignedJWT)
+bestAlg u jwt  = runExceptT $ do
+  let jwk = signingKey jwt
+  alg <- JOSE.bestJWSAlg jwk
+  let claims = encodeJWT u
+  signClaims jwk (newJWSHeader ((), alg)) claims
+
+userToken :: SAS.JWTSettings -> AuthResult AuthenticatedUser -> AppM SignedJWT 
+userToken jwt (SAS.Authenticated u) = do
+  --let jwk = signingKey jwt
+  --alg <- liftIO $ bestAlg jwt 
+  --let claims = encodeJWT u
+  --signed <- signClaims jwk (newJWSHeader ((), alg)) claims
+  signed <- liftIO $ bestAlg u jwt
+  case signed of
+     Left err -> throwError err500
+     -- Right j -> pure $ Book $ cs $ encodeCompact j
+     Right j -> pure j
+  
+userToken _ _ = throwError err401
+  
+userAuthorize :: AuthResult AuthenticatedUser -> AppM () 
+userAuthorize (SAS.Authenticated _adminAuthenticatedUser) = pure () 
+userAuthorize _ = throwError err401
+
+server :: SAS.CookieSettings -> SAS.JWTSettings -> ServerT UserTokenAPI AppM
+server _cookies jwt u = (userToken jwt u) :<|> (userAuthorize u)
+  -- where getBooks :: AppM [Book]
+  --       getBooks = do
+  --         hello <- runRedis $ do
+  --           R.get "hello"
+  --         case hello of
+  --           Right result -> return [Book $ show result] 
+  --           Left _ -> throwError err500
+  --       getSecure :: AuthResult AuthenticatedUser -> AppM [Book]
+  --       getSecure (SAS.Authenticated _adminAuthenticatedUser)  = pure [] 
+  --       getSecure _ = throwError err401 
+
+  
 -- natural transformation from the AppM monad to the Handler
 nt :: AppCtx -> AppM a -> Handler a
 nt s x = runReaderT x s
@@ -112,7 +168,7 @@ app ctx = do
       authCfg = authCheck (getConfiguration ctx) (getConnection ctx)
       cookies = defaultCookieSettings 
       cfg = jwtCfg :. cookies :. authCfg :. EmptyContext
-  pure $ serveWithContext api cfg $ hoistServerWithContext api (Proxy :: Proxy '[BasicAuthData -> IO (AuthResult User), SAS.CookieSettings, SAS.JWTSettings]) (nt ctx) (server cookies jwtCfg)
+  pure $ serveWithContext api cfg $ hoistServerWithContext api (Proxy :: Proxy '[BasicAuthData -> IO (AuthResult AuthenticatedUser), SAS.CookieSettings, SAS.JWTSettings]) (nt ctx) (server cookies jwtCfg)
 
 main :: IO ()
 main = do
